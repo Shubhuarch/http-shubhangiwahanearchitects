8a97ac71e17a5c1915738ec6e369841fe73ae13f
raj0811 <rpbarmaiya@gmail.com>
2024-09-14 01:59:19 +0530

    improve UI

--- src/Pages/HowItWorks.jsx
+++ src/Pages/HowItWorks.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { howWork } from '../Utils/boxes'
 const HowItWorks = () => {
   return (
-    <div className='mt-[8vh] bg-[#ebeae6]'>
+    <div className='mt-[8vh] bg-[#ebeae6] flex flex-col items-center'>
         <div className='text-[#584039] text-[2rem] font-cormorant'>Our Process</div>
 
-        <div  className="flex w-full justify-center mt-[4vh]">
+        <div  className="flex w-[100%] justify-center mt-[4vh]">
         {howWork.map((info,i)=>(
             <div key={i} className="text-center flex flex-col items-center w-[20%] m-2">
                 <div className='w-[30%] h-[12vh]  '>
